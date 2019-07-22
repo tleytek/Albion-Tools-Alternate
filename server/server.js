5ce93fabe941f1d25ddbc2f7fffd49f9575836cc
Mateus Koehler <mateus@mateuskoehler.com>
2019-07-22 15:19:12 -0400

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const next = require('next');
-const NATS = require('nats');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const logger = require('morgan');
+const mongoSessionStore = require('connect-mongo');
+// const expressValidator = require('express-validator');
+const helmet = require('helmet');
+const compression = require('compression');
 const routes = require('./routes');
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
-// const ROOT_URL = dev ? `http://localhost:${port}` : process.env.PRODUCTION_URL
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -29,18 +33,17 @@ mongoose.connection.on('error', err => {
 app.prepare().then(() => {
   const server = express();
 
-  // nats.subscribe('marketorders.deduped', msg => {
-  //   console.log(msg);
-  // });
-  // if (!dev) {
-  //   /* Helmet helps secure our app by setting various HTTP headers */
-  //   server.use(helmet());
-  //   /** Compression gives us gzip compression */
-  //   server.use(compression());
-  // }
+  if (!dev) {
+    /* Helmet helps secure our app by setting various HTTP headers */
+    server.use(helmet());
+    /** Compression gives us gzip compression */
+    server.use(compression());
+  }
 
   /** Body Parser built-in to Express as on version 4.16 */
   server.use(express.json());
+  /* Express Validator will validate form data sent to the backend */
+  // server.use(expressValidator());
 
   /* give all Next.js's requests to Next.js server */
   server.get('/_next/*', (req, res) => {
@@ -51,8 +54,53 @@ app.prepare().then(() => {
     handle(req, res);
   });
 
-  server.use(routes);
+  const MongoStore = mongoSessionStore(session);
+  const sessionConfig = {
+    name: 'next-connect.sid',
+    // secret used for using signed cookies w/ the session
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60 // save session for 14 days
+    }),
+    // forces the session to be saved back to the store
+    resave: false,
+    // don't save unmodified sessions
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 14 // expires in 14 days
+    }
+  };
 
+  if (!dev) {
+    sessionConfig.cookie.secure = true; // serve secure cookies in production environment
+    server.set('trust proxy', 1); // trust first proxy
+  }
+
+  /* Apply our session configuration to express-session */
+  server.use(session(sessionConfig));
+
+  /* morgan for request logging from client
+  - we use skip to ignore static files from _next folder */
+  server.use(
+    logger('dev', {
+      skip: req => req.url.includes('_next')
+    })
+  );
+
+  server.use('/', routes);
+
+  /* Error handling from async / await functions */
+  server.use((err, req, res, next) => {
+    const { status = 500, message } = err;
+    res.status(status).json(message);
+  });
+
+  /* default route
+     - allows Next to handle all other routes
+     - includes the numerous `/_next/...` routes which must    be exposedfor the next app to work correctly
+     - includes 404'ing on unknown routes */
   server.get('*', (req, res) => {
     handle(req, res);
   });
